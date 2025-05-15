a38d621f85c271db4f314541da765576ae7a4a47
Bartek Kozdrowiecki <bkozdrowiecki@divante.pl>
2025-05-15 12:15:28 +0200

    fix: remove key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
   modules: [
     [
       '@storyblok/nuxt', {
-        accessToken: 'z4r3HaOaLnBT2QFO4EraMAtt',
+        accessToken: import.meta.env.VITE_STORYBLOK_TOKEN,
       },
     ],
     '@nuxt/image',
