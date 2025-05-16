2f6082e272d26abc6c64c2f2ec219067bc17b44c
Bartek Kozdrowiecki <bkozdrowiecki@divante.pl>
2025-05-16 14:27:50 +0200

    fix: adjust footer

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -3,22 +3,29 @@
     <div class="grid grid-cols-1 md:grid-cols-3 gap-16">
       <div>
         <HTMLText :content="story?.content?.newsletterContent" />
-        <form class="flex items-center gap-4 mt-20">
+        <form
+          class="flex items-center gap-4 mt-20"
+          @submit.prevent="handleSubmit"
+        >
           <input
             type="email"
-            class="bg-transparent border-b border-gray-600 placeholder-gray-500 outline-none flex-1 py-2"
+            placeholder="Your email"
+            class="bg-transparent border-b border-gray-500 placeholder-gray-500 outline-none flex-1 py-2 focus:px-2 transition-all duration-300"
           />
           <button
             type="submit"
-            class="text-accent font-semibold text-xl transition-transform duration-300 hover:translate-x-1"
+            class="flex items-center justify-center bg-white rounded-full size-16 transition-all duration-300 hover:scale-105 hover:bg-white/80 cursor-pointer"
           >
-            ->
+            <SvgoArrowRight
+              filled
+              class="!mb-0 !size-8 text-black"
+            />
           </button>
         </form>
       </div>
 
       <div class="flex items-center justify-center">
-        <div class="relative bg-white size-40 rounded-full">
+        <div class="relative bg-white size-40 rounded-full shadow-xl">
           <StoryblokImage
             :image="story?.content?.image"
             :webp-format="false"
@@ -45,7 +52,7 @@
       </div>
     </div>
 
-    <div class="mt-36 border-t border-gray-500 pt-8 flex flex-col md:flex-row justify-between gap-6 text-sm">
+    <div class="mt-44 border-t border-gray-500 pt-8 flex flex-col md:flex-row justify-between gap-6 text-sm">
       <p v-text="story?.content?.legalText" />
       <div
         v-if="story?.content?.links?.length"
@@ -56,8 +63,9 @@
           :key="link._uid"
           :to="link.link.url"
           :target="link.link.target"
-          v-text="link.label"
-        />
+        >
+          {{ link.label }}
+        </NuxtLink>
       </div>
     </div>
   </footer>
@@ -67,4 +75,8 @@
 const story = await useAsyncStoryblok('layouts/footer', {
   version: 'draft',
 })
+
+const handleSubmit = (e) => {
+  console.log(e)
+}
 </script>
