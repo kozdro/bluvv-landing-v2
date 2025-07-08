4be6bbbf2a9ab192f048c857148a1489bb6b1b9e
Bartek Kozdrowiecki <bkozdrowiecki@divante.pl>
2025-07-08 21:00:51 +0200

    feat: add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,6 +63,27 @@ export default defineNuxtConfig({
           href: 'https://www.gstatic.com',
           crossorigin: '',
         },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        {
+          rel: 'manifest',
+          href: '/site.webmanifest',
+        },
       ],
     },
   },
